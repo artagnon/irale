291f193625c06248ddf548e70f8aea3139966bd4
Ramkumar Ramachandra <artagnon@gmail.com>
2013-01-19 19:28:56 +0530

    Initial checkin: create a wireframe place-finder
    Use GeoDjango to create a wireframe place finder, with arbitrary
    key-values attached to it.  Also checkin the data for TM world
    borders.
    
    Signed-off-by: Ramkumar Ramachandra <artagnon@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# irale
+
+Backend for a generic place-finding application using PostGIS and
+GeoDjango.  Will ultimately expose a RESTful API, for consumption by
+an Android application.
---
+++ pfinder/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.gis.db import models
+
+class Place(models.Model):
+    name = models.CharField(max_length=255)
+
+    mpoint = models.PointField()
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return self.name
+
+class PlaceToken(models.Model):
+    place = models.ForeignKey('Place')
+
+    key = models.CharField(max_length=255)
+    value = models.CharField(max_length=255)
