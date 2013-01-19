c1c0e89b466f8cdedaf9c2da307002a159aaf01b
Ramkumar Ramachandra <artagnon@gmail.com>
2013-01-19 21:16:55 +0530

    views: add lookupId view
    To look up ^/id/(\d+)/$, create a dummy view.
    
    Signed-off-by: Ramkumar Ramachandra <artagnon@gmail.com>

--- pfinder/views.py
+++ pfinder/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from models import Place, PlaceToken
+from django.http import HttpResponse
+
+def lookupId(request, pid):
+    # Place.objects.filter(placetoken__key__contains=key)
+    return HttpResponse("Will fetch place with id %s." % pid)
+
+def lookupBoundingBox(request):
+    return 1
+
